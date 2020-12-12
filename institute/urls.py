8181f0edd644d56c2a7a994a98d8a4d60d527712
Amjed Saleel K <amjedsaleel@gmail.com>
2020-12-12 14:56:25 +0530

    institute app created and basic settings seted

---
+++ institute/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+# Local Django
+from . import views
+
+app_name = 'institute'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index')
+]
--- institute_management/settings.py
+++ institute_management/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Installed apps
+    'institute.apps.InstituteConfig',
+
+    # Installed via pip
+
 ]
 
 MIDDLEWARE = [
@@ -121,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
