337391160233e02c67b6762411d053de4809f1b7
Amjed Saleel K <amjedsaleel@gmail.com>
2020-12-13 21:19:06 +0530

    institute model created

--- institute/admin.py
+++ institute/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+# Local Django
+from . models import Institute
+
 # Register your models here.
+
+
+class InstituteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'website')
+    list_display_links = ('user', 'name', 'website')
+    search_fields = ('user', 'name', 'short_name')
+
+
+admin.site.register(Institute, InstituteAdmin)
--- institute/models.py
+++ institute/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import ValidationError
+
+# Local django
+from accounts.models import User
 
 # Create your models here.
+
+
+class Institute(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True, help_text='Full name of college')
+    short_name = models.CharField(max_length=15, unique=True, help_text='Short name of college. Spaces are not allowed')
+    website = models.URLField(blank=True)
+    phone_no = models.CharField(max_length=15)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
+
